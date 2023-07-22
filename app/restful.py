e3edd1c7d4425bc2a52cd85ec088f51e0712390e
sandarma190180088 <sandarma.190180088@mhs.unimal.ac.id>
2023-07-23 03:36:13 +0700

    add cnn model

--- app/restful.py
+++ app/restful.py
@@ -1,25 +1,42 @@
 from app import api,app,request
-from flask_restful import Resource
-import joblib
+from keras.models import model_from_json
+from keras.preprocessing.text import Tokenizer
+from keras.utils import pad_sequences
+import pandas as pd
 
-model = joblib.load('model_naive_bayes_v2.sav')
+from flask_restful import Resource
+
+with open('cnn_model.json','r') as m:
+    load_model = m.read()
+
+model = model_from_json(load_model)
+model.load_weights('cnn_weights.h5')
+
+tokenizer = Tokenizer()
+
+X_train = pd.read_csv('X_train_v1.csv')['stopwords']
+tokenizer.fit_on_texts(X_train)
+
+
+
+
+
 class Main(Resource):
     def get(self):
         return {'msg':'Detection Hate Speech v1','status':True},200
     def post(self):
         text = request.form['text']
-        predict = model.predict([text])
-        if predict[0] == 0:
-            resp = 'Hate Speech'
-        elif predict[0] == 1:
-            resp = "Offensive Language"
-        else:
-            resp = "Neither"
+        seq = tokenizer.texts_to_sequences(texts=[text])
+        pad = pad_sequences(seq,maxlen=20)
+        pred = model.predict(pad)
 
-        return {'response':resp,'status':True},200
+        pred = {'hate-speech':float(pred[0][0]),'offensive-lang':float(pred[0][1]),'neither':float(pred[0][2])}
 
 
-api.add_resource(Main,'/hate-speech')
+        return {'response':pred,'status':True},200
+
+
+api.add_resource(Main,'/hate-speech/')
         
         
 
